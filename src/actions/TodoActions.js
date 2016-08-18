0574e13e10cc3c90e0a420ffccc488a4bd9e6a5a
Curt <curt6815463@gmail.com>
2016-08-18 23:24:14 +0000

    v1

--- src/actions/TodoActions.js
+++ src/actions/TodoActions.js
@@ -1,4 +1,26 @@
 // action type
-
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const CLICK_TODO = 'CLICK_TODO';
 
 // action creator
+export function addTodo(text) {
+    return {
+        type:ADD_TODO,
+        text
+    }
+}
+
+export function toggleTodo(index) {
+    return {
+        type:TOGGLE_TODO,
+        index
+    }
+}
+
+export function deleTodo(index) {
+    return {
+        type:CLICK_TODO,
+        index
+    }
+}
--- src/reducers/TodoReducers.js
+++ src/reducers/TodoReducers.js
@@ -1,7 +1,28 @@
+import {ADD_TODO,TOGGLE_TODO,CLICK_TODO} from 'actions/TodoActions'
 
 export function todos(state = [], action) {
   switch (action.type) {
-
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text:action.text,
+          completed:false
+        }
+      ];
+      
+    case TOGGLE_TODO:
+      return [
+        ...state.slice(0,action.index),
+        Object.assign({},state[action.index],{
+          completed: !state[action.index].completed
+        }),
+        ...state.slice(action.index + 1)
+      ];
+    case CLICK_TODO:
+      return [
+        ...state.slice(0,action.index)
+      ]
     default:
       return state;
   }
